8b41a175e9fe36882431ff632d21d1ef44b6dae2
Ayman Mohamed <aymantaha632@gmail.com>
2022-03-07 00:01:34 -0500

    form
    - showing form on map click
    - changing form fields on activity change

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+let map, mapEvent;
+
 navigator.geolocation.getCurrentPosition(
 	function (position) {
 		const { latitude, longitude } = position.coords;
-		const map = L.map("map").setView([latitude, longitude], 13);
+		map = L.map("map").setView([latitude, longitude], 13);
 
 		L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
 			attribution:
@@ -22,23 +24,40 @@ navigator.geolocation.getCurrentPosition(
 		}).addTo(map);
 
 		map.on("click", function (e) {
-			const { lat, lng } = e.latlng;
-			L.marker([lat, lng])
-				.addTo(map)
-				.bindPopup(
-					L.popup({
-						maxWidth: 250,
-						minWidth: 100,
-						autoClose: false,
-						closeOnClick: false,
-						className: "running-popup",
-					})
-				)
-				.setPopupContent("workout!")
-				.openPopup();
+			mapEvent = e;
+			form.classList.remove("hidden");
+			inputDistance.focus();
 		});
 	},
 	function () {
 		alert("Sorry! Location services are not available in the browser");
 	}
 );
+
+form.addEventListener("submit", function (e) {
+	e.preventDefault();
+	inputDistance.value =
+		inputDuration.value =
+		inputCadence.value =
+		inputElevation.value =
+			"";
+	const { lat, lng } = mapEvent.latlng;
+	L.marker([lat, lng])
+		.addTo(map)
+		.bindPopup(
+			L.popup({
+				maxWidth: 250,
+				minWidth: 100,
+				autoClose: false,
+				closeOnClick: false,
+				className: "running-popup",
+			})
+		)
+		.setPopupContent("workout!")
+		.openPopup();
+});
+
+inputType.addEventListener("change", function () {
+	inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+	inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
